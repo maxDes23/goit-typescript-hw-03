cffc6408ee86a93501523b9cefa73c1ec9e51552
maxDes23 <133263504+maxDes23@users.noreply.github.com>
2024-02-01 18:45:04 +0200

    dz

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,41 @@
+class Key {
+  private signature = Math.floor(Math.random() * 9000) + 1000;
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  public door: boolean;
+  public tenants: Person[] = [];
+
+  constructor(public key: Key) {}
+
+  comeIn(person: Person): void {
+    if (!this.door) {
+      return;
+    }
+
+    this.tenants.push(person);
+  }
+
+  openDoor(keyPerson: Key): void {}
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    this.door = key.getSignature() === this.key.getSignature();
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -7,5 +45,4 @@ house.openDoor(person.getKey());
 
 house.comeIn(person);
 
-
-export {};
+export { Key, Person, MyHouse };
